4e46cbc7d93e0c373aee75664fdddc44399675bf
Thanasi <thanasi91@gmail.com>
2012-11-29 17:13:45 -0600

    BUG: Metropolis is not properly sampling

--- Project4/java/Ising.java
+++ Project4/java/Ising.java
@@ -16,7 +16,6 @@
 </html>
 */
 
-
 // Ising.java
 // 2D ising model on a square lattice
 // using Metropolis algorithm
@@ -33,6 +32,8 @@ import P251.*;
 public class Ising extends P251Applet {
     
     /****** Global Variables ******/
+
+    int NAC = 4;  // number of interesting parameters to track in the accumulator
 
     inputPanel ip1,ip2;
     drawPanel dp1;
@@ -53,9 +54,10 @@ public class Ising extends P251Applet {
     // possibilities for spin-flip probabilities
     // only 5 in 2D
     double [] w = new double[5];
-    
+
     int LatticeFill; // how to initialize the lattice - +1, -1, or 0->random
-    int [] accum = new int [5];    // data accumulator for interesting parameters
+    int [] accum = new int [NAC];    // data accumulator
+    String [] accumTitle = new String [NAC];
     int mcs;         // number of mc steps for data
     int nequil;      // number of mc steps for equilibrium
     double accept;   // counter of accepted flips
@@ -93,11 +95,11 @@ public class Ising extends P251Applet {
 	}
     }
 
-
     public void initial () {
 	// initialize the lattice
 	system = new int[L][L];
-	
+	dp1.setDrawBounds(0,0,L,L);
+
 	N = L * L;
 	M = 0;
 	initializeLattice(LatticeFill);
@@ -107,7 +109,6 @@ public class Ising extends P251Applet {
 	for (int i = 0; i<L; i++) {
 	    if (i==L-1) up=0; // toroidal boundaries
 	    else up = i+1;
-	    
 	    for (int j=0; j<L; j++) {
 		if (j==L-1) right=0;
 		else right = j+1;
@@ -126,7 +127,6 @@ public class Ising extends P251Applet {
 
 
     public int getIndex(int dE) {
-
 	if (dE == -8) return 0;
 	else if (dE == -4) return 1;
 	else if (dE == 0) return 2;
@@ -139,7 +139,6 @@ public class Ising extends P251Applet {
     }
 
     public void Metropolis () {
-	
 	for (int t = 0; t<N; t++) {
 	    int i = (int) (Math.random() * L);	
 	    int j = (int) (Math.random() * L);
@@ -158,7 +157,6 @@ public class Ising extends P251Applet {
 
     // calculate the energy for a swap at (i,j) using periodic BC
     public int DeltaE (int i, int j, int L, int [][] system) {
-	
 	if (j==0) left = system[i][L-1];
 	else left = system[i][j-1];
 	if (j==L-1) right = system[i][0];
@@ -173,35 +171,37 @@ public class Ising extends P251Applet {
     } // end DeltaE
 
     public void initialize () {
-	for (int t=0; t<5; t++) accum[t] = 0;
+	for (int t=0; t<NAC; t++) accum[t] = 0;
 	accept = 0;
     } // end initialize
 
     public void data () {
-	accum[0] += E;
-	accum[1] += E*E;
-	accum[2] += M;
-	accum[3] += M*M;
-	accum[4] += Math.abs(M);
+	// calculate the various system properties
+
+	accumTitle[0] = "1";
+	accumTitle[1] = "2";
+	accumTitle[2] = "3";
+	accumTitle[3] = "4";
+
+	accum[0] = system[0][0]; // 
+	accum[1] = system[0][1]; //
+	accum[2] = system[1][0]; //
+	accum[3] = system[1][1]; //
+
     } // end data
 
     public void output () {
-	norm = 1.0 / (mcs * N);
-	accept *= norm;
-	double Eave = accum[0] * norm;
-	double E2ave = accum[1] * norm;
-	double Mave = accum[2] * norm;
-	double M2ave = accum[3] * norm;
-	double absMave = accum[4] * norm;
+	// print out accumulator
+	for (int i = 0; i<NAC; i++) {
+	    System.out.print(String.format("%4.3f\t", 1.0*accum[i]));
+	}
 
-	System.out.println(String.format("%02d\t%4.3f\t%4.3f\t%4.3f\t%4.3f\t%4.3f\t%4.3f", T, accept, Eave, E2ave, Mave, M2ave, absMave));
+	System.out.print("\b\n");
 	
     } // end output
 
 
     public void drawSystem() {
-	int gfxstep = 256 / L;
-
 	// cycle through the lattice
 	for (int i=0; i<L; i++) {
 	    for (int j=0; j<L; j++) {
@@ -209,12 +209,12 @@ public class Ising extends P251Applet {
 		if (system[i][j] == 1) c = Color.DARK_GRAY;
 		else if (system[i][j] == -1) c = Color.CYAN;
 		else c = Color.RED;
-		for (int i2=0; i2<gfxstep; i2++) {
-		    for (int j2=0; j2<gfxstep; j2++) {
-			dp1.addLine(j*gfxstep+j2, i*gfxstep+i2, j*gfxstep+j2, i*gfxstep+i2, c);
-		    }
-		}
-		
+		// for (int i2=0; i2<gfxstep/2; i2++) {
+		    //  for (int j2=0; j2<gfxstep/2; j2++) {
+			// dp1.addLine(j*gfxstep/2+j2, i*gfxstep/2+i2, j*gfxstep/2+j2, i*gfxstep/2+i2, c);
+		dp1.addArc(j, i, .5,.5, 0, 360, c);
+		// }
+		//}
 	    }
 	}
 
@@ -232,14 +232,11 @@ public class Ising extends P251Applet {
 	ip2.addField("nequil", 10);
 	ip2.addField("mcs", 10);
 	
-	dp1 = new drawPanel(256,256, 0,0, 256,256);
+	dp1 = new drawPanel(256,256);
 
 	addPanel(ip2);	
 	addPanel(ip1);
 	addPanel(dp1);
-
-	System.out.println("T\taccept\tEave\tE2ave\tMave\tM2ave\tabsMave");
-	System.out.println("--\t-----\t-----\t-----\t-----\t-----\t-------");
 
     } // end fillPanels
 
@@ -250,8 +247,15 @@ public class Ising extends P251Applet {
 	ip2.setValue(0, 10);
 	ip2.setValue(1, 10);
 
-	System.out.println("\nT\taccept\tEave\tE2ave\tMave\tM2ave\tabsMave");
-	System.out.println("--\t-----\t-----\t-----\t-----\t-----\t-------");
+	System.out.print("\n");
+	for (int i=0; i<NAC; i++) {
+	    System.out.print(accumTitle[i] + "\t");
+	}
+	System.out.print("\b\n");	
+	for (int i=0; i<NAC; i++) {
+	    System.out.print("-----\t");
+	}
+	System.out.print("\b\n");
 
 	readValues();
 	
@@ -263,6 +267,7 @@ public class Ising extends P251Applet {
 	LatticeFill = (int) ip1.getValue(2);
 	nequil = (int) ip2.getValue(0);
 	mcs = (int) ip2.getValue(1);
+
 	initial();
 	
     } // end readValues
